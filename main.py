a91c3929152af69a06c8dacb4747863ef32e918f
Georgy <krytou300@yandex.ru>
2026-07-08 19:17:18 +0300

    Добавили .env

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print "hello world"
-print "New str"
+#!/usr/bin/env python3
+
+print("hello world")
+print("New str")
+
+# Секретики
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+autor = os.getenv('AUTHOR')
+print(autor)
